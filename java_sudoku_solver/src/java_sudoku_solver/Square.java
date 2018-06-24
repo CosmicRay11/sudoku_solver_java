d96cdbc0df8e03582a2ac1e082faf64183590baf
CosmicRay11 <george.w11@hotmail.co.uk>
2018-06-24 18:01:07 +0100

    added methods to allow a square to remove a possibility, to return whether or not it is certain about its value, to return if it can take no value and to return its actual value (i.e. only one possibility for the square)

--- java_sudoku_solver/src/java_sudoku_solver/Square.java
+++ java_sudoku_solver/src/java_sudoku_solver/Square.java
@@ -18,18 +18,52 @@ public final class Square{
             possibilities[i] = true;	
 	}
 	
-	public boolean certain () {
+	public int count_possibilities () {
 		int totalPos = 0;
         for (int i = 0; i <= 8; i++)
             if (possibilities[i] == true) {
             	totalPos = totalPos + 1;
             }
-        if (totalPos == 1) {
-            return true;
-        }
-        else {
-        	return false;
-        }
+        return totalPos;
+	}
+	
+	public void remove_possibility (int num) {
+		// num is the number that has been eliminated as a possibility from the square
+		if (num >= 1 && num <= 9) {
+			int index = num - 1;
+			possibilities[index] = false;
+		}
+	}
+	
+	public int get_value ()  {
+		if (certain()) {
+			for (int i = 0; i <= 8; i++) {
+				if (possibilities[i] == true) {
+					return i+1;
+				}
+			}
+		}
+		// only used to stop eclipse throwing its toys out the pram complaining there's no return int statement
+	    return 0;
+	}
+	
+	public boolean certain () {
+		int count = count_possibilities();
+		if (count == 1) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
+	public boolean impos () {
+		int count = count_possibilities();
+		if (count == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
